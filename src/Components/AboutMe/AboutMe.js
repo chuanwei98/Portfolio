2c0b9952dfaf425276687da46ffe4a8f4062da5b
chuanwei98 <101265042+chuanwei98@users.noreply.github.com>
2024-04-01 18:13:00 +0800

    pages commit

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -11,10 +11,7 @@ export const AboutMe = () => {
             <div>
                 <img src = {Potrait} alt='chad' className='potrait'></img>
                 <p className='aboutMeText'>
-                    I am a self taught Game developer with a big passion for software engineering. I have coded multiple projects on my own such as twitter bots, simulations and other personal coding projects. Some of my personal favourite coding projects were making a compiler and also creating a discord chatbot that could post tweets directly from chat.
-                </p>
-                <p className='aboutMeText'>
-                    I used to be an event organiser and also used to play in the piano ensemble in Singapore Polytechnic. I enjoy playing indie games (independent video games) and discovering new things to learn and do. 
+                    I am a self taught Game developer with a big passion for software engineering. I have coded multiple projects on my own such as twitter bots, simulations and other personal coding projects
                 </p>
                 <p className='aboutMeText'>
                     I graduated from Queen Mary University of London, with first class honours in Computer Science.
